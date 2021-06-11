db6eccbc5cd68e7fcd80a438f54b4ef538d3f8f9
LuanSatiro <satiro_underground@hotmail.com>
2021-06-10 21:18:43 -0300

    amigo secreto

--- frontdesafio/desafio/src/views/users.vue
+++ frontdesafio/desafio/src/views/users.vue
@@ -1,6 +1,16 @@
 <template>
 <div>
-  <h3>Todos os usuários</h3>
+  <h5 class="mt-5">Pessoas para o amigo secreto</h5>
+  <div v-for="user in info" :key="user">
+    {{user.name}}
+  </div>
+  <button @click="sorteio()" class="btn btn-secondary ms-2 mt-3">Sortear </button>
+  <h5 class="mt-5">Resultado sorteio</h5>
+  <div v-for="user in resultado" :key="user">
+    {{user.dar.name}} Tirou {{user.receber.name}}
+    <button @click="sendemail(user)" class="btn btn-secondary ms-2 mt-3">Enviar email</button>
+  </div>
+  <h5 class="mt-5">Alterar usuários</h5>
   <div
       v-for="users in info" :key="users"
       class="users mt-5">
@@ -39,12 +49,13 @@ import axios from 'axios'
 export default {
   name: 'App',
   data () {
-    return{info:null};
+    return{info:null, sorteado:null, resultado:null};
   },
   mounted () {
     axios
       .get('http://localhost:3000/users')
       .then(response => (this.info = response.data))
+      
   },
   methods:{
     delet(id){
@@ -55,7 +66,45 @@ export default {
         }, (error) => {
         console.log(error);
         });
-    }
+    },
+    sorteio(){
+      axios
+      .get('http://localhost:3000/users')
+      .then((response) => {this.sorteado = response.data,
+      this.sorteado.sort(() => (Math.random() > .5) ? 1: -1);
+      var result = []
+      for(let i=0;i<this.sorteado.length-1;++i){
+        result.push({
+          dar:this.sorteado[i],
+          receber:this.sorteado[i+1]
+        })
+      }
+      result.push({
+          dar:this.sorteado[this.sorteado.length-1],
+          receber:this.sorteado[0]
+      })
+      console.log(result)
+      this.resultado=result
+      })
+
+    },
+    sendemail(user){
+        axios.post('http://localhost:3000/users/email', {
+        from: `${user.dar.name} <${user.dar.email}>`,
+        to: `${user.receber.name} <${user.receber.email}>`,
+        subject: 'Amigo Secreto',
+        text: 'Eu tirei você, o que você quer?',
+        html: '<p>Eu tirei você, o que você quer?</p>'
+        }, {
+        headers: {
+            'contentType': 'application/json',
+        }
+        })
+        .then((response) => { console.log(response);
+        alert("Email enviado");}, (error) => {
+        console.log(error);
+        });
+    },
   }
 };
 
